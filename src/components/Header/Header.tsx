4d9a45d885add27d781241a2ca1485da8d0dceb4
EmiSem <just_dragon29@mail.ru>
2025-08-01 13:59:20 +0300

    Added burger menu for mobile devices

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,19 @@ import "./style.scss";
 import logo from "../../assest/images/logo.png";
 import { Navigation } from "../Navigation/Navigation";
 import { Button } from "../Button/Button";
+import { useState } from "react";
 
 export const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    }
+
     return (
         <header className="header">
             <div className="container">
@@ -18,6 +29,13 @@ export const Header = () => {
                         className="header__logo-image"
                     />
                 </div>
+
+                <button className={`header__burger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu} style={{ display: isMenuOpen ? 'none' : 'flex' }}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+
             </div>
             <div className="container">
 
@@ -34,6 +52,67 @@ export const Header = () => {
                     />
                 </div>
             </div>
+
+            {isMenuOpen && (
+                <div className="header__mobile-overlay" onClick={closeMenu}>
+                    <div className="header__mobile-menu" onClick={(e) => e.stopPropagation()}>
+                        <div className="header__mobile-header">
+                            <h2>ЦЕНТР ЛАЗЕРНОЙ МЕДИЦИНЫ</h2>
+                            <button className="header__mobile-close" onClick={closeMenu}>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M18 6L6 18M6 6L18 18" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </button>
+                        </div>
+
+                        <nav className="header__mobile-nav">
+                            <ul className="header__mobile-list">
+                                <li className="header__mobile-item">
+                                    <a href="#" className="header__mobile-link">ВРАЧИ</a>
+                                </li>
+                                <li className="header__mobile-item">
+                                    <a href="#" className="header__mobile-link">УСЛУГИ</a>
+                                </li>
+                                <li className="header__mobile-item">
+                                    <a href="#" className="header__mobile-link">МЕД ТУРИЗМ</a>
+                                </li>
+                                <li className="header__mobile-item">
+                                    <a href="#" className="header__mobile-link">ОМС</a>
+                                </li>
+                                <li className="header__mobile-item">
+                                    <a href="#" className="header__mobile-link">КОНТАКТЫ</a>
+                                </li>
+                                <li className="header__mobile-item header__mobile-item--dropdown">
+                                    <span className="header__mobile-link">ЕЩЕ:</span>
+                                    <ul className="header__mobile-sublist">
+                                        <li className="header__mobile-subitem">
+                                            <a href="#" className="header__mobile-sublink">О КЛИНИКЕ</a>
+                                        </li>
+                                        <li className="header__mobile-subitem">
+                                            <a href="#" className="header__mobile-sublink">ДОКУМЕНТЫ</a>
+                                        </li>
+                                        <li className="header__mobile-subitem">
+                                            <a href="#" className="header__mobile-sublink">ВАКАНСИИ</a>
+                                        </li>
+                                        <li className="header__mobile-subitem">
+                                            <a href="#" className="header__mobile-sublink">ВОПРОСЫ ВРАЧАМ</a>
+                                        </li>
+                                        <li className="header__mobile-subitem">
+                                            <a href="#" className="header__mobile-sublink">НОВОСТИ</a>
+                                        </li>
+                                        <li className="header__mobile-subitem">
+                                            <a href="#" className="header__mobile-sublink">СПРАВОЧНИК БОЛЕЗНЕЙ</a>
+                                        </li>
+                                        <li className="header__mobile-subitem">
+                                            <a href="#" className="header__mobile-sublink">СОТРУДНИЧЕСТВО</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            )}
         </header>
     );
 };
